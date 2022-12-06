0cc5b8e005664365236024674dd775a729516275
Akram Abdeslem Chaima <aslackw@riseup.net>
2022-12-06 23:49:36 +0100

    Add base files

---
+++ 1-git-v2.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+# Author: Erik Dubois <erik.dubois@gmail.com>
+# Maintainer: Akram Abdeslem Chaima <akram@riseup.net>
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+#tput setaf 0 = black 
+#tput setaf 1 = red 
+#tput setaf 2 = green
+#tput setaf 3 = yellow 
+#tput setaf 4 = dark blue 
+#tput setaf 5 = purple
+#tput setaf 6 = cyan 
+#tput setaf 7 = gray 
+#tput setaf 8 = light blue
+##################################################################################################################
+
+# change a commit comment
+# git commit --amend -m "more info"
+# git push --force origin
+
+# checking if I have the latest files from github
+echo "Checking for newer files online first"
+git pull
+
+# Below command will backup everything inside the project folder
+git add --all .
+
+# Give a comment to the commit if you want
+echo "####################################"
+echo "Write your commit comment!"
+echo "####################################"
+
+read input
+
+# Committing to the local repository with a message containing the time details and commit text
+
+git commit -m "$input"
+
+# Push the local files to github
+
+git push -u origin main
+
+
+echo "################################################################"
+echo "###################    Git Push Done      ######################"
+echo "################################################################"
